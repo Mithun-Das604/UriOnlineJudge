4a2f55ad0555b16c95cbda47262ebc38348d375f
Mithun Das <34620044+Mithun-Das604@users.noreply.github.com>
2018-06-04 20:18:21 +0600

    problem solved

---
+++ Problem-1008(Salary)/problem-1008.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+
+    int i,j;
+    double k,result;
+    scanf("%d %d %lf",&i,&j,&k);
+    result=j * k;
+    printf("NUMBER = %d\nSALARY = U$ %.2lf\n",i,result);
+    return 0;
+}
